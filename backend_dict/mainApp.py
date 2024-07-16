9009b7d7764f940b26980619ee7e451528f3f87b
GAGvozdik <69218434+GAGvozdik@users.noreply.github.com>
2024-07-16 16:56:54 +0300

    add addComment page 2 postview blueprint

--- backend_dict/mainApp.py
+++ backend_dict/mainApp.py
@@ -182,96 +182,6 @@ def profile():
 
 
 #################################################################################
-# comments/marks --
-#################################################################################
-
-#TODO add different causes of mark
-@app.route("/poll", methods=['GET', 'POST'])
-def poll():
-    form = PollForm1()
-    form.interested.render_kw = {'style': '', 'class': ''}
-
-    if form.validate_on_submit():
-        if form.interested.data == 'st1' and not form.other2.data:
-            flash('Пожалуйста, укажите причину для выбора "Другое"', category='error')
-
-    return render_template('poll.html', my_text='Опросговна', menu=menu, form=form)
-
-
-# TODO комментарии добавляются на страницу мфк но не добавляются к пользователю на страницу пользователя
-@app.route('/addComment/<alias>', methods=['GET', 'POST'])
-def addComment(alias):
-
-    #TODO use SQLAlchemy 
-    mfk = Mfk.getMfk(alias)
-    if not mfk:
-        abort(404)
-
-    form = starsForm()
-    comments_numb = 0
-    max_comments_numb = 7  # Максимальное количество комментариев
-    current_user_name = current_user.getName()  # Замените на логин текущего пользователя
-
-    # Проверяем, есть ли комментарии пользователя под этой статьей
-    comments = Comments.getComment(alias)
-    if comments:
-        comments_numb = len(comments)
-        for comment in comments:
-            if comment.username == current_user_name:
-                flash('Комментарий уже добавлен','error')
-                return redirect(url_for('postView.showMfk', alias=alias))
-
-    if form.validate_on_submit():
-        # Проверяем, не превышено ли максимальное число комментариев
-        if comments_numb < max_comments_numb:
-            # Извлекаем значения из формы
-            score = form.score.data
-            reason = form.reason.data
-
-            # Преобразуем score в числовое значение
-            score_value = int(score[2:])  # Извлекаем число из 'st5', 'st4', etc.
-            
-            mark_score = Mfk.getMfkScoreNumb(alias) + 1
-
-            res = Comments.addComment(
-                current_user_name,
-                alias,  # Используем alias из URL
-                score_value,  # Передаем числовую оценку
-                mfk.getMfkName(alias),  #TODO change 2 mfktitle  
-                mark_score,
-                reason
-            )
-
-            Mfk.updateMfkScoreNumb(alias, Mfk.getMfkScoreNumb(alias) + 1)
-
-
-            score_list = Comments.getMfkScore(alias)
-            mfk_score = 0
-            len_score_list = 0
-
-            for i in score_list:
-                for j in i:
-                    mfk_score += int(j)
-                    len_score_list += 1
-
-            if len_score_list == 0:
-                mfk_score = 0
-            else:
-                mfk_score = round(mfk_score / len_score_list)
-
-            #TODO сделать id по name 
-            Mfk.updateMfkScore(alias, mfk_score)
-
-            if res:
-                flash('Комментарий добавлен','sucess')
-            else:
-                flash('Ошибка добавления комментария','error')
-        else:
-            flash('Вы достигли лимита коментариев. Удалите комментарий, чтобы добавить новый', category='error')
-
-    return render_template('comments.html', menu=menu, mfk=mfk, alias=alias, my_comments=Comments.getComment(alias), form=form)
-
-#################################################################################
 # login ++
 #################################################################################
 
@@ -485,12 +395,27 @@ def pageNotFount(error):
     return render_template('page404.html', my_text="Страница не найдена", menu=menu), 404
 
 
+#TODO add different causes of mark
+@app.route("/poll", methods=['GET', 'POST'])
+@login_required
+def poll():
+    form = PollForm1()
+    form.interested.render_kw = {'style': '', 'class': ''}
+
+    if form.validate_on_submit():
+        if form.interested.data == 'st1' and not form.other2.data:
+            flash('Пожалуйста, укажите причину для выбора "Другое"', category='error')
+
+    return render_template('poll.html', my_text='Опросговна', form=form)
+
+
 @app.route("/contacts", methods=["POST", "GET"])
 @login_required
 def contacts():
     form = ContactForm()
     form.text.render_kw = {'style': 'height: 200px; width: 350px; ', 'class': 'search-input'}
-    form.recaptcha.render_kw = {'style': 'box-sizing: border-box; display: block; max-width: 100%;'}
+    # TODO re comment recaptcha
+    # form.recaptcha.render_kw = {'style': 'box-sizing: border-box; display: block; max-width: 100%;'}
     if form.validate_on_submit():
         msg = Message(subject='Отзывы и пожелания', sender='MFKStars@gmail.com', recipients=['nagel20@yandex.ru'])
 
--- backend_dict/postview/postview.py
+++ backend_dict/postview/postview.py
@@ -10,7 +10,7 @@ from flask import current_app
 #TODO статьи не отображаются, если их брать из моделей блюпринта
 # from postview.postViewModels import Comments, Mfk
 from Models import Comments, Mfk
-from postview.postViewForms import PollForm1, ContactForm, searchForm, commentDelForm, new_psw_form
+from postview.postViewForms import PollForm1, ContactForm, searchForm, commentDelForm, new_psw_form, starsForm
 
 #TODO clear forms models css
 
@@ -50,3 +50,83 @@ def showMfk(alias):
         abort(404)
 
     return render_template('postview/mfk.html', mfk=mfk, alias=alias, my_comments=Comments.getComment(alias))
+
+
+
+#################################################################################
+# comments/marks --
+#################################################################################
+
+
+# TODO комментарии добавляются на страницу мфк но не добавляются к пользователю на страницу пользователя
+@postView.route('/addComment/<alias>', methods=['GET', 'POST'])
+def addComment(alias):
+
+    #TODO use SQLAlchemy 
+    mfk = Mfk.getMfk(alias)
+    if not mfk:
+        abort(404)
+
+    form = starsForm()
+    comments_numb = 0
+    max_comments_numb = 7  # Максимальное количество комментариев
+    current_user_name = current_user.getName()  # Замените на логин текущего пользователя
+
+    # Проверяем, есть ли комментарии пользователя под этой статьей
+    comments = Comments.getComment(alias)
+    if comments:
+        comments_numb = len(comments)
+        for comment in comments:
+            if comment.username == current_user_name:
+                flash('Комментарий уже добавлен','error')
+                return redirect(url_for('postView.showMfk', alias=alias))
+
+    if form.validate_on_submit():
+        # Проверяем, не превышено ли максимальное число комментариев
+        if comments_numb < max_comments_numb:
+            # Извлекаем значения из формы
+            score = form.score.data
+            reason = form.reason.data
+
+            # Преобразуем score в числовое значение
+            score_value = int(score[2:])  # Извлекаем число из 'st5', 'st4', etc.
+            
+            mark_score = Mfk.getMfkScoreNumb(alias) + 1
+
+            res = Comments.addComment(
+                current_user_name,
+                alias,  # Используем alias из URL
+                score_value,  # Передаем числовую оценку
+                mfk.getMfkName(alias),  #TODO change 2 mfktitle  
+                mark_score,
+                reason
+            )
+
+            Mfk.updateMfkScoreNumb(alias, Mfk.getMfkScoreNumb(alias) + 1)
+
+
+            score_list = Comments.getMfkScore(alias)
+            mfk_score = 0
+            len_score_list = 0
+
+            for i in score_list:
+                for j in i:
+                    mfk_score += int(j)
+                    len_score_list += 1
+
+            if len_score_list == 0:
+                mfk_score = 0
+            else:
+                mfk_score = round(mfk_score / len_score_list)
+
+            #TODO сделать id по name 
+            Mfk.updateMfkScore(alias, mfk_score)
+
+            if res:
+                flash('Комментарий добавлен','sucess')
+            else:
+                flash('Ошибка добавления комментария','error')
+        else:
+            flash('Вы достигли лимита коментариев. Удалите комментарий, чтобы добавить новый', category='error')
+
+    return render_template('postview/comments.html', mfk=mfk, alias=alias, my_comments=Comments.getComment(alias), form=form)
